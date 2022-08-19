34f48a078553fc0747e0e1069ba44e6728251b42
Lyssa <lyssa.lima@poli.digital>
2022-08-19 12:23:33 -0300

    requisições iniciais

---
+++ curl.php
@@ -0,0 +1,20 @@
+<?php
+    //inicia a conexão
+    $curl = curl_init();
+
+    //define as configurações
+ 
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "http://localhost/aprendendo-curl/api.php", //passa a url desejada
+        CURLOPT_CUSTOMREQUEST => "GET", //tipo da requisição
+        CURLOPT_RETURNTRANSFER => true //tipo da referencia 
+
+    ]);
+    
+    //executa a requisição
+    $response = curl_exec($curl);
+
+    //fecha a conexão
+    curl_close($curl);
+
+    echo $response;
